3f0fed70ab1ad19bd27fa3781f45395888b2bad4
svmcdude <34194571+svmcdude@users.noreply.github.com>
2018-03-21 17:39:37 +0530

    Delete db_config.php

--- db_config.php
+++
@@ -1,9 +0,0 @@
-<?php   
-  $servername = "127.0.0.1";
-  $username = "root";
-  $password = "";
-  $dbname = "feedie_base";
-            
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-?>
